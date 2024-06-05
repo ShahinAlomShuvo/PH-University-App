f87c9872b0d2f7f8031887b4e9362541b3c0c2a9
Shahin Alom Shuvo <shahinalomshuvo94@gmail.com>
2024-06-05 12:33:36 +0600

    implement handle zod error

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
+  env: process.env.NODE_ENV,
   db: process.env.DB_URL,
   port: process.env.PORT,
   bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS,
--- src/middleware/errorHandler.middleware.ts
+++ src/middleware/errorHandler.middleware.ts
@@ -1,18 +1,45 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSource } from "../interface/errors.interface";
+import config from "../config";
 
-const errorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const status = err.status || httpStatus.INTERNAL_SERVER_ERROR;
-  const message = err.message || "Something went wrong";
-  return res.status(status).send({
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let status = err.status || httpStatus.INTERNAL_SERVER_ERROR;
+  let message = err.message || "Something went wrong";
+
+  let errorSource: TErrorSource = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const status = httpStatus.BAD_REQUEST;
+    const message = "Validation error";
+    const errorSource: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+
+    return { status, message, errorSource };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    status = simplifiedError?.status;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
+  }
+
+  return res.status(status).json({
     success: false,
     message,
-    error: err.errors || err,
+    errorSource,
+    stack: config.env === "development" ? err.stack : null,
   });
 };
 
